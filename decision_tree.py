7b807a03ed5e6136569ccee6cf13364a1d58ef00
Brian Schendt <brianschendt@gmail.com>
2021-01-26 08:17:57 -0500

    decision tree basic implementation

---
+++ decision_tree.py
@@ -0,0 +1,16 @@
+import preprocessing
+from sklearn.tree import DecisionTreeClassifier # Import Decision Tree Classifier
+from sklearn.model_selection import train_test_split # Import train_test_split function
+from sklearn import metrics #Import scikit-learn metrics module for accuracy calculation
+
+df = preprocessing.preprocess()
+X = df[preprocessing.feature_columns]
+y = df[preprocessing.label_column]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1)
+
+clf = DecisionTreeClassifier()
+clf = clf.fit(X_train,y_train)
+y_pred = clf.predict(X_test)
+
+print("Accuracy:", metrics.accuracy_score(y_test, y_pred))
--- preprocessing.py
+++ preprocessing.py
@@ -1,7 +1,10 @@
 import pandas as pd
 
+DATA_FILENAME = "data/cmc-data.csv"
+data_headers = ["Wife's Age", "Wife's Education", "Husband's Education", "Number of children ever born", "Wife's religion", "Wife's now working?", "Husbands's occupation", "Standard-of-living index", "Media exposure", "Contraceptive method used"]
+feature_columns = data_headers[0:-1]
+label_column = data_headers[-1]
+
 def preprocess():
-    DATA_FILENAME = "data/cmc-data.csv"
-    data_headers = ["Wife's Age", "Wife's Education", "Husband's Education", "Number of children ever born", "Wife's religion", "Wife's now working?", "Husbands's occupation", "Standard-of-living index", "Media exposure", "Contraceptive method used"]
     df = pd.read_csv(DATA_FILENAME, sep=',', names=data_headers)
     return df
